239e63ef03cb2bf2bd0d531404b62cd0b4ce68c8
Orion Lowy <orionlowy@gmail.com>
2020-02-10 16:42:45 -0600

    MongoRepository implementation with test case

---
+++ api/src/main/java/com/sdp/flightapi/dao/FlightRepository.java
@@ -0,0 +1,13 @@
+package com.sdp.flightapi.dao;
+
+import com.sdp.flightapi.models.ReservedFlights;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "flights", path = "flights")
+public interface FlightRepository extends MongoRepository<ReservedFlights, String> {
+    List<ReservedFlights> findByOriginName(@Param("name") String name);
+}
