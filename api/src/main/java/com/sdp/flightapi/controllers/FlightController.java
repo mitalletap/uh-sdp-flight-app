17af5226bd7adbbca8b941c78bdf3916f0ae48af
kevin huynh <kevinhuynh269@yahoo.com>
2020-03-04 22:59:47 -0600

    Added filtering by Date and Price

--- api/src/main/java/com/sdp/flightapi/controllers/FlightController.java
+++ api/src/main/java/com/sdp/flightapi/controllers/FlightController.java
@@ -6,8 +6,8 @@ import com.sdp.flightapi.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.Comparator;
 
 @RequestMapping("api")
 @RestController
@@ -39,11 +39,57 @@ public class FlightController {
     @PostMapping(path = "/post-reserved-flight")
     public String saveFlight(@RequestBody ReservedFlights reservedFlights){
         flightDao.save(reservedFlights);
-        return "Added reserved flight with info " + reservedFlights.getId();
+        return "Added reserved :flight with info " + reservedFlights.getId();
     }
+
+    @GetMapping(path = "/get-reserved-flight-fliter-ascending-sort-price")
+    public List<ReservedFlights> FlightFilterPrice(){
+//ASCENDING SORT
+        ArrayList<ReservedFlights> ReservedFlightsHolder = (ArrayList<ReservedFlights>) flightDao.findAll();
+        Collections.sort(ReservedFlightsHolder, (Comparator<ReservedFlights>) (r1, r2) -> {
+        return Float.valueOf(r1.getPrice()).compareTo(r2.getPrice());
+        }
+        );
+        return ReservedFlightsHolder;
+    }
+    @GetMapping(path = "/get-reserved-flight-fliter-descending-sort-price")
+    public List<ReservedFlights> FlightFilterPriceDescending(){
+//ASCENDING SORT
+        ArrayList<ReservedFlights> ReservedFlightsHolder = (ArrayList<ReservedFlights>) flightDao.findAll();
+        Collections.sort(ReservedFlightsHolder, (Comparator<ReservedFlights>) (r1, r2) -> {
+                    return Float.valueOf(r2.getPrice()).compareTo(r1.getPrice());
+                }
+        );
+        return ReservedFlightsHolder;
+    }
+
+    @GetMapping(path = "/get-reserved-flight-filter-date-descending")
+    public List<ReservedFlights> FlightFilterDateDescending(){
+        ArrayList<ReservedFlights> ReservedFlightsHolder = (ArrayList<ReservedFlights>) flightDao.findAll();
+        Collections.sort(ReservedFlightsHolder, (Comparator<ReservedFlights>) (r1, r2) -> {
+            return String.valueOf(r2.getOutboundDepartureDate()).compareTo(r1.getOutboundDepartureDate());
+                }
+
+        );
+        return ReservedFlightsHolder;
+    }
+
+    @GetMapping(path = "/get-reserved-flight-filter-date-ascending")
+    public List<ReservedFlights> FlightFilterDateAscending(){
+        ArrayList<ReservedFlights> ReservedFlightsHolder = (ArrayList<ReservedFlights>) flightDao.findAll();
+        Collections.sort(ReservedFlightsHolder, (Comparator<ReservedFlights>) (r1, r2) -> {
+                    return String.valueOf(r1.getOutboundDepartureDate()).compareTo(r2.getOutboundDepartureDate());
+                }
+
+        );
+        return ReservedFlightsHolder;
+    }
+
+
 
     @GetMapping(path = "/get-reserved-flights")
     public List<ReservedFlights> getReservedFlights(){
+
         return flightDao.findAll();
     }
 
