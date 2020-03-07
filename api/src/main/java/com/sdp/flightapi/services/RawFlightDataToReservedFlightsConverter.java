434e370adcc9b56eb2e542d2d0e7b61c64b16abd
Orion Lowy <orionlowy@gmail.com>
2020-03-06 22:03:39 -0600

    Removed default-constructor from RawFlightDataToReservedFlightsConverter

--- api/src/main/java/com/sdp/flightapi/services/RawFlightDataToReservedFlightsConverter.java
+++ api/src/main/java/com/sdp/flightapi/services/RawFlightDataToReservedFlightsConverter.java
@@ -7,8 +7,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class RawFlightDataToReservedFlightsConverter {
-    public RawFlightDataToReservedFlightsConverter() {}
-
     public List<ReservedFlights> convert(RawFlightData rawFlightData) {
         List<ReservedFlights> formattedFlights
                 = buildFlightListFromQuotesAndCarriers(rawFlightData);
