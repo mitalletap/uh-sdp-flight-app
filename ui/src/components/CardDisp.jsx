7d8351cf12e15476563b3c9a517c389d4d21bb8a
mitul323 <33381456+mitul323@users.noreply.github.com>
2020-03-10 01:34:55 -0500

    switched to react table instead of html

--- ui/src/components/CardDisp.jsx
+++ ui/src/components/CardDisp.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Table } from "rsuite";
+import { Content } from "rsuite";
+import Table from "react-bootstrap/Table";
 import jsonData from "../data/actualData";
 import { Card, Button, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const { Column, HeaderCell, Cell } = Table;
+// const { Column, HeaderCell, Cell } = Table;
 var loadData = JSON.parse(JSON.stringify(jsonData));
 
 class CardDisp extends Component {
@@ -39,7 +40,52 @@ class CardDisp extends Component {
               {places[0].CityName}->{places[1].CityName}
             </Card.Title>
             <Card.Text>
-              <Table autoHeight width={1200} data={[loadData]}>
+              <Table striped bordered hover variant="dark">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Airliner</th>
+                    <th>Origin Airport</th>
+                    <th>Destination Airport</th>
+                    <th>Departure Time</th>
+                    <th>Arrival Time</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>{carriers[1].Name}</td>
+                    <td>{places[0].CityName}</td>
+                    <td>{places[1].CityName}</td>
+                    <td>{quotes[0].OutboundLeg.DepartureDate}</td>
+                    <td>{quotes[0].OutboundLeg.DepartureDate}</td>
+                    <td>
+                      {currency[0].Symbol} {quotes[0].MinPrice}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </Table>
+
+              {/* <Table autoHeight width={1200} data={[loadData]}>
                 <Column width={200}>
                   <HeaderCell>Airliner</HeaderCell>
                   <Cell dataKey="Name">{carriers[1].Name}</Cell>
@@ -76,12 +122,11 @@ class CardDisp extends Component {
                     {quotes[0].MinPrice}
                   </Cell>
                 </Column>
-              </Table>
+              </Table>*/}
             </Card.Text>
             <Button variant="primary">Book now</Button>
           </Card.Body>
         </Card>
-        <br />
       </div>
     );
   }
--- ui/src/pages/Card.jsx
+++ ui/src/pages/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import FlightSearch from "../components/FlightSearch";
+
 import CardDisp from "../components/CardDisp";
-// import NavigationBar from "../components/NavigationBar";
-import { Container, Footer, Content } from "rsuite";
+import NavigationBar from "../components/NavigationBar";
+import { Container, Header, Footer, Content } from "rsuite";
 
 const Card = () => {
   return (
     <React.Fragment>
       <Container>
+        {/* <Header>
+          <NavigationBar />
+        </Header> */}
         <Content>
           <CardDisp />
         </Content>
