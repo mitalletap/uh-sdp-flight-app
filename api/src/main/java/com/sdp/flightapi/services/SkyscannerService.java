c8731012a2531f11a4bcfeef62b10b12f964dfdd
Nemo Robles <nemo@Nemos-MacBook-Pro.local>
2020-03-16 14:03:53 -0500

    skyscannerService using browseroutes

--- api/src/main/java/com/sdp/flightapi/services/SkyscannerService.java
+++ api/src/main/java/com/sdp/flightapi/services/SkyscannerService.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class SkyscannerService {
     private final WebClient webClient;
     private final String flightSearchBaseUrl = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/" +
-            "apiservices/browsequotes/v1.0/US/USD/en-US/";
+            "apiservices/browseroutes/v1.0/US/USD/en-US/";
     private final String xRapidApiKey = "c25f6b34acmsh3e88e6211d976dcp1b322cjsn2b02ff0fa923";
 
     public SkyscannerService(WebClient.Builder webClientBuilder) {
